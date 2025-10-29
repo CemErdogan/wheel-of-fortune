40ab22773a9bfbf32dfa0bc15d0d76e867045eba
CemErdogan <cem.cemerdogan97@gmail.com>
2025-10-29 20:01:40 +0300

    Update zone visual

--- Assets/Systems/WheelOfFortuneSystem/Scripts/Runtime/View/ZoneItemView.cs
+++ Assets/Systems/WheelOfFortuneSystem/Scripts/Runtime/View/ZoneItemView.cs
@@ -9,14 +9,13 @@ namespace WheelOfFortuneSystem
     public class ZoneItemView : MonoBehaviour, IZoneItemView
     {
         [SerializeField, ValidateNotNull] private TextMeshProUGUI indexText;
-        [SerializeField, ValidateNotNull] private Image iconImage;
         
         [Inject] private readonly ZoneAreaConfig _config;
         
         public void SetIndex(int index)
         {
             indexText.SetText(index.ToString());
-            iconImage.color = _config.GetColorByIndex(index);
+            indexText.color = _config.GetColorByIndex(index);
         }
 
         private void OnValidate()
