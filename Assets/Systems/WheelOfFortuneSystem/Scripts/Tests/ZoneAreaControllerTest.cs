d315c64ae766049df3bd4048f8b48ef844b82cd2
CemErdogan <cem.cemerdogan97@gmail.com>
2025-10-29 16:45:26 +0300

    Add zone tests

---
+++ Assets/Systems/WheelOfFortuneSystem/Scripts/Tests/ZoneAreaControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using Zenject;
+using NSubstitute;
+using UnityEngine;
+
+namespace WheelOfFortuneSystem.Tests
+{
+    [TestFixture]
+    public class ZoneAreaControllerTest : ZenjectUnitTestFixture
+    {
+        private IZoneAreaController _controller;
+        private IZoneAreaModel _model;
+        private IZoneAreaView _view;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _model = Substitute.For<IZoneAreaModel>();
+            _view = Substitute.For<IZoneAreaView>();
+
+            Container.Bind<IZoneAreaModel>().FromInstance(_model).AsSingle();
+            Container.Bind<IZoneAreaView>().FromInstance(_view).AsSingle();
+            Container.Bind<IZoneAreaController>().To<FakeZoneAreaController>().AsSingle();
+
+            Container.Inject(this);
+            _controller = Container.Resolve<IZoneAreaController>();
+
+            Assert.That(_controller.Model, Is.EqualTo(_model), "Model should be same instance");
+            Assert.That(_controller.View, Is.EqualTo(_view), "View should be same instance");
+        }
+
+        [Test]
+        public void SetZoneIndex_Should_Update_Model_And_Snap_View()
+        {
+            var fakeTarget = CreateFakeRectTransform();
+
+            _controller.SetZoneIndex(5, fakeTarget);
+
+            _model.Received(1).CurrentZoneIndex = 5;
+            _view.Received(1).SnapTo(fakeTarget);
+        }
+
+        private RectTransform CreateFakeRectTransform()
+        {
+            var go = new GameObject("FakeRectTransform");
+            return go.AddComponent<RectTransform>();
+        }
+        
+        private class FakeZoneAreaController : IZoneAreaController
+        { 
+            public IZoneAreaModel Model { get; }
+            public IZoneAreaView View { get; }
+            
+            public FakeZoneAreaController(IZoneAreaModel model, IZoneAreaView view)
+            {
+                Model = model;
+                View = view;
+            }
+            
+            public void Init() { }
+
+            public void SetZoneIndex(int currentZoneIndex, RectTransform target)
+            {
+                Model.CurrentZoneIndex = currentZoneIndex;
+                View.SnapTo(target);
+            }
+        }
+    }
+}
---
+++ Assets/Systems/WheelOfFortuneSystem/Scripts/Tests/ZoneItemControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using Zenject;
+using NSubstitute;
+
+namespace WheelOfFortuneSystem.Tests
+{
+    [TestFixture]
+    public class ZoneItemControllerTest : ZenjectUnitTestFixture
+    {
+        private IZoneItemController _controller;
+        private IZoneItemModel _model;
+        private IZoneItemView _view;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _model = Substitute.For<IZoneItemModel>();
+            _view = Substitute.For<IZoneItemView>();
+
+            Container.Bind<IZoneItemModel>().FromInstance(_model).AsSingle();
+            Container.Bind<IZoneItemView>().FromInstance(_view).AsSingle();
+            Container.Bind<IZoneItemController>().To<FakeZoneItemController>().AsSingle();
+
+            Container.Inject(this);
+            _controller = Container.Resolve<IZoneItemController>();
+
+            Assert.That(_controller.Model, Is.EqualTo(_model), "Model should be same instance");
+            Assert.That(_controller.View, Is.EqualTo(_view), "View should be same instance");
+        }
+
+        [Test]
+        public void Prepare_Should_Update_Model_And_Call_View()
+        {
+            _controller.Prepare(7);
+
+            _model.Received(1).Index = 7;
+            _view.Received(1).SetIndex(7);
+        }
+
+        private class FakeZoneItemController : IZoneItemController
+        {
+            public IZoneItemModel Model { get; }
+            public IZoneItemView View { get; }
+
+            public FakeZoneItemController(IZoneItemModel model, IZoneItemView view)
+            {
+                Model = model;
+                View = view;
+            }
+
+            public void Prepare(int index)
+            {
+                Model.Index = index;
+                View.SetIndex(index);
+            }
+        }
+    }
+}
