55febe8a2f870c8969372d9c34777da6ce9de115
CemErdogan <cem.cemerdogan97@gmail.com>
2025-10-29 16:31:23 +0300

    Add zone change

--- Assets/Systems/WheelOfFortuneSystem/Scripts/Runtime/Core/ZoneArea.cs
+++ Assets/Systems/WheelOfFortuneSystem/Scripts/Runtime/Core/ZoneArea.cs
@@ -8,14 +8,14 @@ namespace WheelOfFortuneSystem
         [Inject] private readonly IZoneAreaController _controller;
         [Inject] private readonly IZoneAreaView _view;
         [Inject] private readonly IZoneAreaModel _model;
-        
+
         private ZoneItem[] _zoneItems;
 
         private void Start()
         {
             _controller.Init();
         }
-        
+
         public void SetZoneItems(ZoneItem[] zoneItems)
         {
             _zoneItems = zoneItems;
@@ -30,18 +30,38 @@ namespace WheelOfFortuneSystem
         {
             return _view.GetItemSpacing();
         }
-        
+
         public int GetCurrentZon()
         {
             return _model.CurrentZoneIndex;
         }
-        
-        public void SetZoneIndex(int currentZoneIndex)
+
+        public void SetZoneIndex(int nextZoneIndex)
         {
-            var targetItem = _zoneItems[currentZoneIndex % _zoneItems.Length];
-            _controller.SetZoneIndex(currentZoneIndex, targetItem.GetComponent<RectTransform>());
+            while (_model.CurrentZoneIndex < nextZoneIndex)
+            {
+                StepForwardOnce();
+            }
         }
-        
+
+        private void StepForwardOnce()
+        {
+            _model.CurrentZoneIndex++;
+
+            var leftMost = _zoneItems[0];
+            for (int i = 1; i < _zoneItems.Length; i++)
+            {
+                _zoneItems[i - 1] = _zoneItems[i];
+            }
+            _zoneItems[^1] = leftMost;
+
+            var rt = leftMost.transform;
+            rt.SetAsLastSibling();
+
+            var newIndexForRightMost = _model.CurrentZoneIndex + _zoneItems.Length - 1;
+            leftMost.SetIndex(newIndexForRightMost + 1);
+        }
+
         public class Factory : PlaceholderFactory<ZoneArea> { }
     }
 }
--- Assets/Systems/WheelOfFortuneSystem/Scripts/Runtime/Core/ZoneItem.cs
+++ Assets/Systems/WheelOfFortuneSystem/Scripts/Runtime/Core/ZoneItem.cs
@@ -5,12 +5,18 @@ namespace WheelOfFortuneSystem
 {
     public class ZoneItem : MonoBehaviour
     {
-        [Inject] private readonly IZoneItemController _zoneItemController;
+        [Inject] private readonly IZoneItemController _controller;
+        [Inject] private readonly IZoneItemView _view;
         
         public void Prepare(RectTransform parent, int index)
         {
             transform.SetParent(parent);
-            _zoneItemController.Prepare(index);
+            _controller.Prepare(index);
+        }
+        
+        public void SetIndex(int newIndexForRightMost)
+        {
+            _view.SetIndex(newIndexForRightMost);
         }
 
         public class Factory : PlaceholderFactory<ZoneItem>
